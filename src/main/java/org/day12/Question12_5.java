55b5fbdacc3dd75143a3a4235783066fae3c4148
2-dean <limsjs@naver.com>
2023-07-08 19:44:31 +0900

    day12 끝

---
+++ src/main/java/org/day12/Question12_5.java
@@ -0,0 +1,44 @@
+package org.day12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Question12_5 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 2, 1, 10, 2, 1};
+        int[] query = {4, 1, 2};
+        int[] result = solution(arr, query);
+
+        System.out.println("√ result : " + result);
+    }
+
+
+
+    public static int[] solution(int[] arr, int[] query) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i<arr.length; i++) {
+            list.add(arr[i]);
+        }
+
+        for (int i=0; i< arr.length; i++) {
+
+            if (i % 2 == 0){
+                //짝수
+               arr = Arrays.copyOfRange(arr,0,query[i]+1);
+            } else {
+                // 홀수 인덱스
+                arr = Arrays.copyOfRange(arr,query[i],arr.length);
+            }
+        }
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+            System.out.println("list : " + list.get(i));
+        }
+        return answer;
+    }
+
+} //class
