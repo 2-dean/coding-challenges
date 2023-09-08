c07b3ef91bfcbdaad10031e871cd008ffefa1ac1
ddu <limsjs@naver.com>
2023-09-08 17:00:07 +0900

    백준 ing

---
+++ src/main/java/org/baekjoon/string/Main_1157.java
@@ -0,0 +1,22 @@
+package org.baekjoon.string;
+
+public class Main_1157 {
+    /*
+    * 문제 :
+    * 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오.
+    *  단, 대문자와 소문자를 구분하지 않는다.
+     * */
+    public static String solution(String str) {
+        String answer = "";
+
+        for (char c : str.toCharArray()) {
+            // 맵에 넣고, VALUE값을 비교
+            // value가 큰값 return
+            // valuer가 큰 값이 2개이상이면 ? 리턴
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution("Mississipi"));
+    }
+}
---
+++ src/main/java/org/template/Main_0.java
@@ -0,0 +1,11 @@
+package org.template;
+
+public class Main_0 {
+
+    public static void solution(String str) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
