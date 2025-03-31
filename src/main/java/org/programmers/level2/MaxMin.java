128d679b4e80f621d23c1aa6b86fd3c78c04d104
ddu <limsjs@naver.com>
2025-03-31 14:39:35 +0900

    250331_최댓값과 최솟값

--- src/main/java/org/programmers/level2/MaxMin.java
+++ src/main/java/org/programmers/level2/MaxMin.java
@@ -7,18 +7,26 @@ public class MaxMin {
     public static String solution(String s) {
 
         String[] arr = s.split(" ");
-        int[]  nums = new int[arr.length];
+/*        int[]  nums = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
             nums[i] = Integer.parseInt(arr[i]);
         }
 
         Arrays.sort(nums);
         int max = nums[nums.length - 1];
-        int min = nums[0];
+        int min = nums[0];*/
 
+        int max = Integer.MIN_VALUE; // 제일 작은 수를 저장
+        int min = Integer.MAX_VALUE; // 제일 큰 수를 저장
 
+        for (String num : arr) {
+            int n = Integer.parseInt(num);
+            min = Math.min(min, n);
+            max = Math.max(max, n);
+        }
         String answer = min + " " + max;
+
+
         return answer;
     }
 
